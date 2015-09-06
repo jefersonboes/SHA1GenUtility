28a47edd7389bef7f4d7e277ce6ea3fad902f4be
Jeferson <jefersonboes@gmail.com>
2015-09-05 21:44:44 -0300

    added icon

--- FormMain.Designer.cs
+++ FormMain.Designer.cs
@@ -47,6 +47,7 @@ namespace SHA1GenUtility
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormMain));
             this.labelFile = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
@@ -67,6 +68,7 @@ namespace SHA1GenUtility
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(284, 261);
             this.Controls.Add(this.labelFile);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "FormMain";
             this.Text = "SHA1GenUtility";
             this.DragDrop += new System.Windows.Forms.DragEventHandler(this.Form1_DragDrop);
